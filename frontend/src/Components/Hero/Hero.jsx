695d0e55552e7a32de3a6e9133ea3abf0937d0d3
Arslan Shaukat <eskillspoint@gmail.com>
2023-12-28 01:04:12 +0500

    Hero to Popular items

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -1,17 +1,36 @@
 import React from 'react';
 import './Hero.css';
-import PropTypes from 'prop-types';
+
+import hand_icon from '../Assets/hand_icon.png';
+import arrow_icons from '../Assets/arrow.png'
+import hero_image from '../Assets/hero_image.png'
 
 const Hero = () => {
   return (
     <div className='hero'>
-      {/* Your component code here */}
+      <div className="hero-left">
+        <h2>NEW ARRIVALS ONLY</h2>
+        <div>
+          <div className="hero-hand-icon">
+            <p>new</p>
+            <img src={hand_icon} alt="" />
+          </div>
+          <p>Collections</p>
+          <p>for everyone</p>
+        </div>
+        <div className="hero-latest-btn">
+          <div>Latest Collections</div>
+          <img src={arrow_icons} alt="" />
+        </div>
+      </div>
+      <div className="hero-right">
+        <img src={hero_image} alt="" />
+
+      </div>
     </div>
   );
 };
 
-Hero.propTypes = {
-  // Define your prop types here
-};
+
 
 export default Hero;
